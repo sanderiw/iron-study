3ff0c0ea8fd3d0c2419f79f8321ca2098773cdae
Sander Iwase <sanderiw@gmail.com>
2021-09-10 18:49:55 -0300

    Importar bootstrap e font awesome

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,28 @@
+const card = [
+    {
+        "url" : "https://www.youtube.com/watch?v=Ws9WVHhNq5M",
+        "type": "video",
+        "text": "Encontrei um vídeo sobre React para iniciantes",
+        "tag": "#react #js",
+        "author": "Natalia Alves",
+        "createdTime": "10-09-2021",
+    },
+    {
+        "url" : "https://medium.com/rafaeltardivo/git-github-uma-introdução-prática-fed91ee56b8d",
+        "type": "article",
+        "text": "Mto boa essa dica de git e github",
+        "tag": "#html #css",
+        "author": "Sander Iwase",
+        "createdTime": "08-09-2021",
+    },
+    {
+        "url" : "https://www.youtube.com/watch?v=6itmqFMFUdA",
+        "type": "video",
+        "text": "Video interessante na minha timeline do YT",
+        "tag": "#interessante #vida",
+        "author": "Anna",
+        "createdTime": "06-09-2021",
+    }
+]
+
+export default card;
