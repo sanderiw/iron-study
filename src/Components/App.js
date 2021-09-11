7ddc74fa5c4e1324cc8b49541f777765e7b35c06
Natália Alves <natalia.alvescam@gmail.com>
2021-09-11 11:11:57 -0300

    feita barra de pesquisa - ainda sem filtragem dinâmica

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import card from './Card/Card';
+import Searchbar from './Homepage/Searchbar'
 
 
 function App() {
   return (
     <div>
+      <Searchbar/>
     </div>
   );
 }
